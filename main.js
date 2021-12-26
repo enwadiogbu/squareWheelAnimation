671cd3b3f8e4c7113b3f48bfcf44674a12c91c6d
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2021-12-25 21:48:03 -0700

    first commit setting up bare skeleton of Three.js

---
+++ main.js
@@ -0,0 +1,34 @@
+//RENDERER SETUP
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector(".webGLContainer").appendChild(renderer.domElement);
+
+//SCENE SETUP
+const scene = new THREE.Scene();
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(0, 5, 10);
+camera.lookAt(0, 0, 0);
+
+const cubeGeom = new THREE.BoxGeometry(1, 1, 3);
+const cubeMat = new THREE.MeshNormalMaterial();
+const cubeMesh = new THREE.Mesh(cubeGeom, cubeMat);
+scene.add(cubeMesh);
+
+//RENDER LOOP
+function update() {
+  cubeMesh.rotation.x += 0.01;
+  cubeMesh.rotation.y += 0.01;
+  cubeMesh.scale.y = Math.sin(Date.now() * 0.01);
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(update);
+}
+
+update();
