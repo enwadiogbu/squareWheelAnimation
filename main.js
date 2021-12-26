8f14f04885515d2fb618b5282c7dfad989e60047
Emeka Nwadiogbu, Jr <emekanwadiogbu.jr@me.com>
2021-12-25 23:43:46 -0700

    For loop, while loop clone, and animation on x and y rotation axis with camera positioning Three.js

--- main.js
+++ main.js
@@ -13,20 +13,42 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0, 5, 10);
+camera.position.set(0, 5, 30);
 camera.lookAt(0, 0, 0);
 
-const cubeGeom = new THREE.BoxGeometry(1, 1, 3);
-const cubeMat = new THREE.MeshNormalMaterial();
-const cubeMesh = new THREE.Mesh(cubeGeom, cubeMat);
-scene.add(cubeMesh);
+let cubeMesh;
+const cubeArmy = new THREE.Group();
+
+const modelLoader = new THREE.GLTFLoader();
+modelLoader.load("BlenderProjects/squareWheel.glb", (glb) => {
+  console.log(glb);
+  glb.scene.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      cubeMesh = child;
+      cubeMesh.material = new THREE.MeshNormalMaterial();
+    }
+  });
+  for (let i = 0; i < 20; i++) {
+    const c = cubeMesh.clone();
+    c.rotation.y = Math.PI / 2;
+    c.scale.set(i, i, i);
+    cubeArmy.add(c);
+  }
+
+  scene.add(cubeArmy);
+});
 
 //RENDER LOOP
 function update() {
-  cubeMesh.rotation.x += 0.01;
-  cubeMesh.rotation.y += 0.01;
-  cubeMesh.scale.y = Math.sin(Date.now() * 0.01);
+  cubeArmy.rotation.y += 0.01;
+  cubeArmy.rotation.x += 0.01;
 
+  let i = 0;
+  while (i < cubeArmy.children.length) {
+    cubeArmy.children[i].rotation.x += 0.01 + i * 0.001;
+    cubeArmy.children[i].rotation.y += 0.01 + i * 0.001;
+    i++;
+  }
   renderer.render(scene, camera);
   requestAnimationFrame(update);
 }
